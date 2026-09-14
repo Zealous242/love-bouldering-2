c1f37d5989a2370ada12ed00187d6e344bdbb72d
Zealous242 <yasum.abbas137@gmail.com>
2026-09-14 13:54:36 +0100

    Update SECRET_KEY in settings.py

--- boulderWiki/settings.py
+++ boulderWiki/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vt71aet@a)gaum=+wvzn506wz+lgk=f&8(m_hy5=c0)h_9887&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
