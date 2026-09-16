6853ad6d4121fe0a6e200a5521783ca3cb8927f4
Zealous242 <yasum.abbas137@gmail.com>
2026-09-16 12:14:55 +0100

    Install summernote 0.8.20.0 and customize admin portal

--- boulderWiki/urls.py
+++ boulderWiki/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("article.urls"), name="article-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
 ]
