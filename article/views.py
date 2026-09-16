0534892beeec5c4abc0f15b4d101437f4824f187
Zealous242 <yasum.abbas137@gmail.com>
2026-09-16 20:04:21 +0100

    Add page navigation arrows in index.html and add a favicon in base.html

--- article/views.py
+++ article/views.py
@@ -6,6 +6,6 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "article/index.html"
-    paginate_by = 6
+    paginate_by = 3
     
     
